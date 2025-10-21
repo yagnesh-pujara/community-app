00213a932cad3933b61978b5ba52fa3614523293
yagnesh-pujara <yagneshpujara05@gmail.com>
2025-10-21 20:06:06 +0530

    Frontend Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // <-- this is important
+  },
   server: {
     port: 3000,
     proxy: {
